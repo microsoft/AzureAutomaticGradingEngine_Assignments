beddb0695347ca59cddb5682a2b38825af460781
cyrus wong <cywong@vtc.edu.hk>
2021-09-20 16:59:29 +0800

    Completed Vnet test.

---
+++ AzureProjectGrader/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureProjectGrader
+{
+    public class Constants
+    {
+        public static readonly string ResourceGroupName = "projProd";
+        public static readonly string Vnet1Name = "projVnet1Prod";
+        public static readonly string Vnet2Name = "projVnet2Prod";
+
+    }
+}
