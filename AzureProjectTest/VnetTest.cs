29b26aff88fef6627f13cf8a0dbae8c5a3fc8abb
Cyrus Wong <cywong@vtc.edu.hk>
2022-08-17 06:57:33 +0000

    Fix Test14_Vnet1PrivateSubnetNetworkSecurityGroup and Test10_Vnet1PublicSubnetsNatGateway. Fix grammar. Add more game tasks.

--- AzureProjectTest/VnetTest.cs
+++ AzureProjectTest/VnetTest.cs
@@ -10,7 +10,7 @@ namespace AzureProjectTest;
 public class VnetTests
 {
     [GameTask(
-        "Can you help create 2 vnet? First vnet named 'projVnet1Prod' in 'southeastasia' of CIDR '10.0.0.0/16' and " +
+        "Can you create 2 vnets? First vnet named 'projVnet1Prod' in 'southeastasia' of CIDR '10.0.0.0/16' and " +
         "Second vnet named 'projVnet2Prod' in 'southeastasia' of CIDR '10.1.0.0/16'.",
         2, 10, 1)]
     [Test]
@@ -32,6 +32,10 @@ public class VnetTests
         Assert.AreEqual("10.1.0.0/16", scope.vnet2.AddressSpace.AddressPrefixes[0], "Vnet2 Address space 10.1.0.0/16");
     }
 
+    [GameTask(
+    "Can you create 2 subnets in vnet named 'projVnet1Prod? CIDR 10.0.1.0/24 and 10.0.0.0/24. Then" +
+        " create 2 subnets in vnet named 'projVnet2Prod' CIDR 10.1.1.0/24 and 10.1.0.0/24",
+    5, 20, 2)]
     [Test]
     public void Test03_VnetWith2Subnets()
     {
@@ -40,6 +44,7 @@ public class VnetTests
         Assert.AreEqual(2, scope.vnet2.Subnets.Count, "2 subnets");
     }
 
+    [GameTask(2)]
     [Test]
     public void Test04_Vnet1SubnetsCidr()
     {
@@ -50,7 +55,7 @@ public class VnetTests
         Assert.IsNotNull(privateSubnet);
     }
 
-
+    [GameTask(2)]
     [Test]
     public void Test05_Vnet2SubnetsCidr()
     {
@@ -61,6 +66,7 @@ public class VnetTests
         Assert.IsNotNull(privateSubnet);
     }
 
+    [GameTask("Can you set 2 routes in vnet named 'projVnet1Prod VnetLocal and Internet for 10.0.1.0/24 subnet? To make it as a public subnet.", 5, 10)]
     [Test]
     public void Test06_Vnet1PublicSubnetsRoutes()
     {
@@ -80,6 +86,7 @@ public class VnetTests
         Assert.IsNotNull(internetRoute);
     }
 
+    [GameTask("Can you set 2 routes in vnet named 'projVnet2Prod VnetLocal and Internet for 10.1.1.0/24 subnet? To make it as a public subnet.", 5, 10)]
     [Test]
     public void Test07_Vnet2PublicSubnetsRoutes()
     {
@@ -99,7 +106,7 @@ public class VnetTests
         Assert.IsNotNull(internetRoute);
     }
 
-
+    [GameTask("Can you set 2 routes in vnet named 'projVnet1Prod VnetLocal for 10.0.0.0/24 subnet? To prepare it as a private subnet.", 5, 10)]
     [Test]
     public void Test08_Vnet1PrivateSubnetsRoutes()
     {
@@ -114,6 +121,7 @@ public class VnetTests
         Assert.IsNotNull(localRoute);
     }
 
+    [GameTask("Can you set 2 routes in vnet named 'projVnet2Prod VnetLocal for 10.1.0.0/24 subnet? To prepare it as a private subnet.", 5, 10)]
 
     [Test]
     public void Test09_Vnet2PrivateSubnetsRoutes()
@@ -129,22 +137,26 @@ public class VnetTests
         Assert.IsNotNull(localRoute);
     }
 
+    [GameTask("Can you add a Standard NAT Gateway at zone 1 for subnet 10.0.1.0/24 ? ", 5, 10)]
 
     [Test]
-    public void Test10_Vnet1PrivateSubnetsNatGateway()
+    public void Test10_Vnet1PublicSubnetsNatGateway()
     {
         using var scope = new TestScope();
-        var privateSubnet = scope.GetVnet1PrivateSubnet();
-        privateSubnet = scope.client.Subnets.Get(Constants.ResourceGroupName, scope.vnet1.Name, privateSubnet.Name,
+        var publicSubnet = scope.GetVnet1PublicSubnet();
+        publicSubnet = scope.client.Subnets.Get(Constants.ResourceGroupName, scope.vnet1.Name, publicSubnet.Name,
             "NatGateway");
-        Assert.IsNotNull(privateSubnet.NatGateway);
-        var natGatewayId = privateSubnet.NatGateway.Id;
+        Assert.IsNotNull(publicSubnet.NatGateway);
+        var natGatewayId = publicSubnet.NatGateway.Id;
         var natGateways = scope.client.NatGateways.List(Constants.ResourceGroupName);
         var natGateway = natGateways.FirstOrDefault(c => c.Id == natGatewayId);
 
         Assert.AreEqual("Standard", natGateway.Sku.Name);
         Assert.AreEqual("1", natGateway.Zones[0]);
     }
+
+    [GameTask("Can you add a Virtual Network Peering from projVnet1Prod to projVnet2Prod (Remote)? " +
+        "Allow Forwarded Traffic and Virtual Network Access but not allow Gateway Transit.", 5, 10)]
 
     [Test]
     public void Test11_VnetGlobalPeering()
@@ -158,6 +170,9 @@ public class VnetTests
         Assert.IsFalse(virtualNetworkPeering.AllowGatewayTransit);
     }
 
+    [GameTask("Can you add 2 Network Security Rules to subnet 10.0.1.0/24? " +
+        "First rule allows connect to HTTP from anywhere with priority 201." +
+        "Second rule allows all TCP outbound to anywhere with priority 100.", 5, 10)]
     [Test]
     public void Test12_Vnet1PublicSubnetNetworkSecurityGroup()
     {
@@ -189,6 +204,9 @@ public class VnetTests
         Assert.AreEqual("*", allowAllTcpOutbound.DestinationAddressPrefix);
     }
 
+    [GameTask("Can you add 2 Network Security Rules to subnet 10.1.1.0/24? " +
+    "First rule allows connect to HTTP from anywhere with priority 201." +
+    "Second rule allows all TCP outbound to anywhere with priority 100.", 5, 10)]
     [Test]
     public void Test13_Vnet2PublicSubnetNetworkSecurityGroup()
     {
@@ -219,14 +237,18 @@ public class VnetTests
         Assert.AreEqual("*", allowAllTcpOutbound.DestinationAddressPrefix);
     }
 
+    [GameTask("Can you add 2 Network Security Rules to subnet 10.1.1.0/24? " +
+"First rule allows HTTP cross vent in bound from 10.1.0.0/24." +
+"Second rule allows all TCP outbound to anywhere with priority 100.", 5, 10)]
     [Test]
     public void Test14_Vnet1PrivateSubnetNetworkSecurityGroup()
     {
         using var scope = new TestScope();
-        var privateSubnet = scope.GetVnet1PrivateSubnet();
-        privateSubnet = scope.client.Subnets.Get(Constants.ResourceGroupName, scope.vnet1.Name, privateSubnet.Name,
+        var privateSubnet1 = scope.GetVnet1PrivateSubnet();
+        var privateSubnet2 = scope.GetVnet2PrivateSubnet();
+        privateSubnet1 = scope.client.Subnets.Get(Constants.ResourceGroupName, scope.vnet1.Name, privateSubnet1.Name,
             "NetworkSecurityGroup");
-        var networkSecurityGroup = privateSubnet.NetworkSecurityGroup;
+        var networkSecurityGroup = privateSubnet1.NetworkSecurityGroup;
         Assert.IsNotNull(networkSecurityGroup);
 
         var allowAllTcpOutbound = networkSecurityGroup.SecurityRules.FirstOrDefault(c => c.DestinationPortRange == "*");
@@ -246,8 +268,8 @@ public class VnetTests
         Assert.AreEqual("TCP", crossVnetInbound.Protocol.ToUpper());
         Assert.AreEqual("80", crossVnetInbound.DestinationPortRange);
         Assert.AreEqual(201, crossVnetInbound.Priority);
-        Assert.IsTrue(privateSubnet.AddressPrefix == crossVnetInbound.DestinationAddressPrefix ||
-                      privateSubnet.AddressPrefix == crossVnetInbound.DestinationAddressPrefixes[0]);
+        Assert.IsTrue(privateSubnet2.AddressPrefix == crossVnetInbound.DestinationAddressPrefix ||
+                      privateSubnet2.AddressPrefix == crossVnetInbound.DestinationAddressPrefixes[0]);
     }
 
     [Test]
