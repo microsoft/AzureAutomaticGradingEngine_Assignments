58043a4a61b70ff35c498cf8b03b58e3cba59731
Cyrus Wong <cywong@vtc.edu.hk>
2022-09-15 03:44:08 +0000

    Remove unnecessary ToList.

--- GameTaskGeneratorConsoleApp/Program.cs
+++ GameTaskGeneratorConsoleApp/Program.cs
@@ -56,7 +56,7 @@ var serializerSettings = new JsonSerializerSettings
 {
     ContractResolver = new CamelCasePropertyNamesContractResolver()
 };
-allTasks = allTasks.ToList().OrderBy(c => c.GameClassOrder).ToList();
+allTasks = allTasks.OrderBy(c => c.GameClassOrder).ToList();
 var json = JsonConvert.SerializeObject(allTasks.ToArray(), serializerSettings);
 Console.WriteLine(json);
 File.WriteAllText(@"tasks.json", json);
